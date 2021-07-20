5e2e66dba5a9bd8973fbd39cd9e267dddb66dc49
Joshua onwuzu <joshuaonwuzu75@gmail.com>
2021-07-20 11:18:46 -0700

    google-oauth-updated

--- app.js
+++ app.js
@@ -10,20 +10,27 @@ const session = require('express-session')
 const passport = require("passport");
 const passportLocalMongoose =  require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const findOrCreate = require("mongoose-findorcreate")
+const findOrCreate = require("mongoose-findorcreate");
+
 
 // Package  Configuration
 const app = express()
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+
+
 app.use(session({
     secret: 'penticosta is real',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
   }))
 app.use(passport.initialize());
 app.use(passport.session());
+
+
+  
 
 
 mongoose.connect("mongodb://localhost:27017/UsersDB",{useNewUrlParser : true, useUnifiedTopology:true});
@@ -31,29 +38,46 @@ mongoose.set("useCreateIndex", true)
 const saltRounds = 5;
 const userSchema = new mongoose.Schema({
     username : String,
-    password : String
+    password : String,
+    googleId : String,
+    secret : String
 })
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
 const User = mongoose.model("User", userSchema);
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
 
+
+
+
+
+
+
+// Implementing Google Oauth 
+
+passport.serializeUser((user, done)=>{
+    done(null,user.id);
+})
+passport.deserializeUser((id, done)=>{
+    User.findById(id).then((user)=>{
+        done(null,user.id)
+    })
+})
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/wuzu",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
     },
-  function(accessToken, refreshToken, profile, cb) {
+  function(accessToken, refreshToken, profile, done) {
+      console.log(profile)
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
+      return done(err, user);
     });
   }
-));
+))
 
 // Request Handlers
 
@@ -61,9 +85,14 @@ app.get("/", (req, res)=>{
     res.render("home");
 });
 
-app.get("/auth/google",(req,res)=>{
-    passport.authenticate('google', { scope: ['profile'] });
-})
+app.get("/auth/google",passport.authenticate('google', { scope: ['profile']}));
+
+app.get('/auth/google/wuzu', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 
 app.route("/register")
 .get((req, res)=>{
@@ -113,12 +142,45 @@ app.get("/logout",(req, res)=>{
 })
 
 app.get("/secrets", (req, res)=>{
- if (req.isAuthenticated()){
-     res.render("secrets");
- }else{
-     res.redirect("/login")
- }
-})      
+//  if (req.isAuthenticated()){
+//      res.render("secrets");
+//  }else{
+//      res.redirect("/login")
+//  }
+   User.find({"secret" : {$ne : null}},(err, data)=>{
+    if (!err){
+        if (data){
+            res.render("secrets", {userSecrets : data})
+        }
+        else{
+            console.log(" i didnt find any data")
+        }
+        
+    }  else {
+        console.log(err)
+    } 
+   })
+   
+}) 
+
+app.get("/submit", (req,res)=>{
+    if (req.isAuthenticated()){
+        res.render("submit");
+    }else{
+        res.redirect("/login")
+    }
+})
+app.post("/submit", (req, res)=>{
+    const submittedSecret = req.body.secret;
+    User.findById (req.user, (err,data)=>{
+        if (!err){
+            data.secret = submittedSecret;
+            data.save(() =>{
+                res.redirect("/secrets")
+            })
+        }
+    })
+})
 app.listen("3000", ()=>{
     console.log("Server is up and running")
 })
